0e991238b2f3e266ba5b4d8e2e0f7d74c93eb16b
Prasanna <prasanna@hexaware.com>
2020-10-29 10:26:33 +0530

    code

---
+++ java_exam_prep/java/P15.java
@@ -0,0 +1,16 @@
+public class P15 {
+    public static void main(String[] args) {
+    //     int i = 1; 
+    // int j = 0; 
+    // j = i++ + 1;  
+    // System.out.println(j);
+    // System.out.println(i);
+    
+    int i = 1; 
+    int j = 0;
+    j = ++i + 1;   
+    System.out.println();
+    System.out.println(j);
+    System.out.println(i);
+    }
+}
---
+++ java_exam_prep/java/P20.java
@@ -0,0 +1,16 @@
+public class P20 {
+    public static void main(String[] args) {
+        // byte b1;  
+        // b1 = 25 + 110; 
+    
+        // System.out.println(b1);
+        
+        int x=133;
+        byte b = (byte)x;
+        System.out.println(b);
+        b+=3;
+        System.out.println(b);
+        // b1 = b1 + 210; 
+        // System.out.println(b1);
+    }
+}
